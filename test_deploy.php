21525bcb928d8555161ce3c0b5476fd15ce32735
Jordan-dito <75227714+Jordan-dito@users.noreply.github.com>
2026-01-12 23:51:17 -0500

    chore: limpieza de archivos de prueba tras exito en despliegue

--- test_deploy.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>¡Éxito! El despliegue automático está funcionando 🚀</h1>";
-echo "<p>Si estás leyendo esto desde tu dominio real, significa que GitHub le avisó a tu servidor y este descargó los cambios automáticamente.</p>";
-echo "<p><b>Hora de actualización:</b> " . date('Y-m-d H:i:s') . "</p>";
-?>
